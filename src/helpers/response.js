429b502a04e45f1c87b65a3233f1bb9f25d89738
Clinton Agada <clinton.agada@andela.com>
2019-06-25 16:56:53 +0100

    create the phone generator API and add tests

---
+++ src/helpers/response.js
@@ -0,0 +1,15 @@
+import ApplicationError from './Error';
+import { sentenceCase } from '.';
+
+export default {
+  empty: resource =>
+    new ApplicationError(
+      `Oops, looks like no ${resource} exist yet! Generate some first!`,
+      404
+    ),
+  success: (resource, action, data = {}) => ({
+    status: 'success',
+    message: `Awesome! ${sentenceCase(`${resource} ${action} successfully`)}`,
+    data,
+  }),
+};
